67368d2494a27285d8c9a8b428e9b0eb1bc953cb
Roby Putera Adhiguna Rantung <goriifast3235@gmail.com>
2022-12-13 15:09:13 +0700

    Add Player Movement Logic & Animation
    Animation : Idle, Running

--- Dungeon Escape/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField] private float _speed = 10.0f;
+    [SerializeField] private LayerMask groundLayer; 
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        Movement();
+    }
 
-        rb.velocity = new Vector2(horizontalInput, rb.velocity.y);
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(horizontalInput * _speed * Time.deltaTime, _rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && CheckGround(true))
+        {
+            float jumpForce = 5.0f;
+            
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+            CheckGround(false);
+        }
+    }
+
+    bool CheckGround(bool isGrounded)
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 0.1f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return isGrounded = true;
+        }
+        return isGrounded = false;
     }
 }
